a1b81459c75ce7ab85743a6ba7895de50ceb4609
Unknown <unknown@example.com>
2020-11-22 23:47:05 -0500

    ConnectionComponent: *UI

--- src/main/java/com/jhw/module/util/rest_config/ui/connection/ConnectionComponent.java
+++ src/main/java/com/jhw/module/util/rest_config/ui/connection/ConnectionComponent.java
@@ -14,6 +14,7 @@ import com.jhw.swing.material.components.labels.MaterialLabelsFactory;
 import com.jhw.swing.material.injection.MaterialSwingInjector;
 import com.jhw.swing.material.standards.MaterialColors;
 import com.jhw.swing.material.standards.MaterialFontRoboto;
+import com.jhw.swing.util.interfaces.MaterialComponent;
 import com.jhw.utils.interfaces.Update;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -31,8 +32,8 @@ public class ConnectionComponent extends _PanelGradient implements Update {
     public static final String TEXT_CONNECTED = "CONECTADO";
     public static final String TEXT_NOT_CONNECTED = "SIN CONECCION";
 
-    public static final Color COLOR_CONNECTED = MaterialColors.GREENA_200;
-    public static final Color COLOR_NOT_CONNECTED = MaterialColors.REDA_200;
+    public static final Color COLOR_CONNECTED = MaterialColors.GREEN_900;
+    public static final Color COLOR_NOT_CONNECTED = MaterialColors.RED_900;
 
     public static MaterialPanelBorder fromComponent() {
         return MaterialSwingInjector.getImplementation(ConnectionComponent.class);
@@ -42,6 +43,8 @@ public class ConnectionComponent extends _PanelGradient implements Update {
         update();
     });
 
+    private boolean lastState = false;
+
     public ConnectionComponent() {
         initComponents();
         timer.start();
@@ -50,9 +53,12 @@ public class ConnectionComponent extends _PanelGradient implements Update {
     private void initComponents() {
         labelConnection = MaterialLabelsFactory.build();
         labelConnection.setHorizontalAlignment(SwingConstants.CENTER);
-        labelConnection.setFont(MaterialFontRoboto.MEDIUM.deriveFont(10f));
+        labelConnection.setFont(MaterialFontRoboto.BOLD.deriveFont(12f));
 
+        this.setToolTipText("Estado de la conección hacia el servidor");
         this.setLayout(new BorderLayout());
+        this.setBackground(MaterialColors.TRANSPARENT);
+        //this.setBorderThickness(2);
 
         this.add(labelConnection);
     }
@@ -60,8 +66,17 @@ public class ConnectionComponent extends _PanelGradient implements Update {
 
     @Override
     public void update() {
-        setBackground(RESTHandler.isConnected() ? COLOR_CONNECTED : COLOR_NOT_CONNECTED);
-        labelConnection.setText(RESTHandler.isConnected() ? TEXT_CONNECTED : TEXT_NOT_CONNECTED);
+        boolean actualState = RESTHandler.isConnected();
+
+        if (actualState != lastState) {
+            lastState = actualState;
+
+            Color c = actualState ? COLOR_CONNECTED : COLOR_NOT_CONNECTED;
+            labelConnection.setForeground(c);
+            this.setBorderColor(c);
+
+            labelConnection.setText(actualState ? TEXT_CONNECTED : TEXT_NOT_CONNECTED);
+        }
     }
 
 }
