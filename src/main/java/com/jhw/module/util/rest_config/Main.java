866afa6bc9d5e65022126ec278986b542d51b11a
Unknown <unknown@example.com>
2020-12-17 15:33:26 -0500

    Main: test OAuth2RestTemplate

--- src/main/java/com/jhw/module/util/rest_config/Main.java
+++ src/main/java/com/jhw/module/util/rest_config/Main.java
@@ -5,12 +5,8 @@
  */
 package com.jhw.module.util.rest_config;
 
-import com.clean.core.app.services.ExceptionHandler;
 import com.jhw.module.util.rest_config.services.RESTHandler;
-import com.jhw.module.util.rest_config.ui.connection.ConnectionComponent;
-import com.jhw.module.util.rest_config.ui.module.RestConfigSwingModule;
-import com.jhw.swing.bundles.dialog.DialogPanel;
-import org.springframework.web.client.RestTemplate;
+import java.util.Scanner;
 
 /**
  *
@@ -22,7 +18,22 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        new DialogPanel("123123123", ConnectionComponent.fromComponent()).setResizable(true);
+        System.out.println(RESTHandler.login("admin", "admin"));
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            System.out.println("");
+            String endpoint = in.next();
+            if (endpoint.equals("break")) {
+                break;
+            } else if (endpoint.equals("token")) {
+                System.out.println(RESTHandler.OAuth2RestTemplate().getAccessToken());
+                continue;
+            }
+            System.out.println(RESTHandler.OAuth2RestTemplate().getForObject("http://localhost:8080/test/" + endpoint, String.class));
+            System.out.println("");
+        }
+
     }
 
 }
