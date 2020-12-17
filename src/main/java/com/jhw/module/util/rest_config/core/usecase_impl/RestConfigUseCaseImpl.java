ef40bfd1f365e805354bcbdc03bae1ccd09563e3
Unknown <unknown@example.com>
2020-12-17 15:15:11 -0500

    RestConfigUseCaseImpl: +oauth2Template

--- src/main/java/com/jhw/module/util/rest_config/core/usecase_impl/RestConfigUseCaseImpl.java
+++ src/main/java/com/jhw/module/util/rest_config/core/usecase_impl/RestConfigUseCaseImpl.java
@@ -1,18 +1,17 @@
 package com.jhw.module.util.rest_config.core.usecase_impl;
 
+import com.jhw.module.util.rest_config.core.utils.RestOperationsFactory;
 import com.clean.core.app.usecase.DefaultReadWriteUseCase;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jhw.module.util.rest_config.core.domain.Configuration;
 import com.jhw.module.util.rest_config.core.domain.ConnectionTarget;
 import com.jhw.module.util.rest_config.core.module.RestConfigCoreModule;
-import javax.inject.Inject;
 import java.util.List;
 import com.jhw.module.util.rest_config.core.repo_def.RestConfigRepo;
 import com.jhw.module.util.rest_config.core.usecase_def.RestConfigUseCase;
-import com.jhw.utils.jackson.JACKSON;
+import com.jhw.module.util.rest_config.core.utils.OAuth2RestOperationsFactory;
 import com.jhw.utils.others.Red;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.web.client.RestOperations;
 
 public class RestConfigUseCaseImpl extends DefaultReadWriteUseCase<Configuration> implements RestConfigUseCase {
 
@@ -21,15 +20,16 @@ public class RestConfigUseCaseImpl extends DefaultReadWriteUseCase<Configuration
      */
     private final RestConfigRepo repo = RestConfigCoreModule.getInstance().getImplementation(RestConfigRepo.class);
 
-    private final RestTemplate template;
+    private final RestOperations template;
+    private OAuth2RestOperations oauth2Template;
 
     /**
      * Constructor por defecto, usado par injectar.
      */
-    @Inject
     public RestConfigUseCaseImpl() {
         super.setRepo(repo);
-        template = initRestTemplate();
+        template = RestOperationsFactory.from(this).build();
+        //oauth2Template = OAuth2RestOperationsFactory.from(this).build();
     }
 
     @Override
@@ -43,23 +43,13 @@ public class RestConfigUseCaseImpl extends DefaultReadWriteUseCase<Configuration
     }
 
     @Override
-    public RestTemplate restTemplate() throws Exception {//TODO
+    public RestOperations restTemplate() throws Exception {//TODO
         return template;
     }
 
-    private RestTemplate initRestTemplate() {
-        ObjectMapper om = new ObjectMapper();
-        JACKSON.configureObjectMapper(om);
-
-        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-        messageConverter.setPrettyPrint(true);
-        messageConverter.setObjectMapper(om);
-
-        RestTemplate rt = new RestTemplate();
-        rt.getMessageConverters().removeIf(m -> m.getClass().isAssignableFrom(MappingJackson2HttpMessageConverter.class));
-        rt.getMessageConverters().add(messageConverter);
-
-        return rt;
+    @Override
+    public OAuth2RestOperations OAuth2RestTemplate() {
+        return oauth2Template;
     }
 
     @Override
@@ -70,9 +60,4 @@ public class RestConfigUseCaseImpl extends DefaultReadWriteUseCase<Configuration
             return false;
         }
     }
-
-    /*@Override
-    public RestTemplate restTemplate() throws Exception {//TODO
-        return new RestTemplate();
-    }*/
 }
