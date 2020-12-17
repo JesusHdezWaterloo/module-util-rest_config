1f97a0dc182feb3f5f812063ee18ba19a6758a11
Unknown <unknown@example.com>
2020-12-17 15:14:26 -0500

    RestConfigUseCase: +OAuth2RestTemplate

--- src/main/java/com/jhw/module/util/rest_config/core/usecase_def/RestConfigUseCase.java
+++ src/main/java/com/jhw/module/util/rest_config/core/usecase_def/RestConfigUseCase.java
@@ -4,7 +4,8 @@ import com.clean.core.app.usecase.ReadWriteUseCase;
 import com.jhw.module.util.rest_config.core.domain.Configuration;
 import com.jhw.module.util.rest_config.core.domain.ConnectionTarget;
 import java.util.List;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.web.client.RestOperations;
 
 /**
  * Interfaz del caso de uso de la licencia para definir las principales
@@ -18,7 +19,9 @@ public interface RestConfigUseCase extends ReadWriteUseCase<Configuration> {
 
     public ConnectionTarget actual() throws Exception;
 
-    public RestTemplate restTemplate() throws Exception;
+    public RestOperations restTemplate() throws Exception;
+
+    public OAuth2RestOperations OAuth2RestTemplate() throws Exception;
 
     public boolean isConnected();
 
